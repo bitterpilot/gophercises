5649f2de71928f657f3acfd7ac2c397fb2e32afb
Nathan Minissale <nathanminissale@gmail.com>
2019-03-12 10:23:07 +0800

    Used the handler to serve the story

--- cyoa/cmd/web/main.go
+++ cyoa/cmd/web/main.go
@@ -4,12 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/bitterpilot/gophercises/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "The port to start CYOA web application on")
 	filename := flag.String("file", "./gopher.json", "The JSON file with the cyoa story")
 	flag.Parse()
 	fmt.Printf("Using the file %s\n", *filename)
@@ -21,9 +23,13 @@ func main() {
 	}
 
 	//decode file
-	Story, err := cyoa.JsonStory(f)
+	story, err := cyoa.JsonStory(f)
 	if err != nil {
 		log.Fatalf("%s\n", err)
 	}
-	fmt.Printf("%+v\n", Story)
+
+	// start webserver
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
