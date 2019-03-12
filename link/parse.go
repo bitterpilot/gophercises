a22bade0dd862432af9ff3707652fe8dc14dc726
Nathan Minissale <nathanminissale@gmail.com>
2019-03-12 18:18:36 +0800

    Added a Depth First Search function

--- link/parse.go
+++ link/parse.go
@@ -1,7 +1,10 @@
 package link
 
 import (
+	"fmt"
 	"io"
+
+	"golang.org/x/net/html"
 )
 
 // Link is a simplified version of <a href="..."> link
@@ -12,5 +15,17 @@ type Link struct {
 
 // Parse takes a HTML document and returns the links in a simplified struct.
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	fmt.Println(padding, n.Data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, "  ")
+	}
 }
