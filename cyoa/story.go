059a46ca0d24acdbaad27d6db82e780f28d0e855
Nathan Minissale <nathanminissale@gmail.com>
2019-03-12 11:50:10 +0800

    Added some styling to the template

--- cyoa/story.go
+++ cyoa/story.go
@@ -38,16 +38,58 @@ var DefultHandlerTpl = `
     <title>Choose Your Own Adventure</title>
 </head>
 <body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-        <p>{{.}}</p>
-    {{end}}
-    <ul>
-    {{range .Options}}
-        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-    {{end}}
-    </ul>
+	<section class="page">
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+			<p>{{.}}</p>
+		{{end}}
+		<ul>
+		{{range .Options}}
+			<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+		{{end}}
+		</ul>
+	</section>
 </body>
+<style>
+	body {
+		font-family: helvetica, arial;
+	}
+	h1 {
+		text-align:center;
+		position:relative;
+	}
+	.page {
+		width: 80%;
+		max-width: 500px;
+		margin: auto;
+		margin-top: 40px;
+		margin-bottom: 40px;
+		padding: 80px;
+		background: #FFFCF6;
+		border: 1px solid #eee;
+		box-shadow: 0 10px 6px -6px #777;
+	}
+	ul {
+		border-top: 1px dotted #ccc;
+		padding: 10px 0 0 0;
+		-webkit-padding-start: 0;
+	}
+	li {
+		padding-top: 10px;
+	}
+	a,
+	a:visited {
+		text-decoration: none;
+		color: #6295b5;
+	}
+	a:active,
+	a:hover {
+		color: #7792a2;
+	}
+	p {
+		text-indent: 1em;
+	}
+	</style>
 </html>
 `
 
@@ -74,7 +116,7 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if chapter, ok := h.s[path]; ok {
 		if err := tpl.Execute(w, chapter); err != nil {
 			log.Printf("%v\n", err)
-		    http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
 		}
 		return
 	}
